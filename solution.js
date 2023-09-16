a43a89a459d8d407632256af804be1eeaffc7265
Chantal <chantalgomez@pursuit.org>
2023-09-15 20:00:13 -0400

    Finally understanding the questions. Done correctly

--- solution.js
+++ solution.js
@@ -2,145 +2,68 @@ const { nums, words } = require("./data/data.js");
 
 // Every
 const isEveryNumGreaterThan2 = (nums) => {
- 
-  for (let i = 0; i < nums.length; i++){
-    if( nums[i] < 2 ){
-      return false
-    }else{
-      return true
-    }
-  }
+  return nums.every(num => num > 2);
 };
 
 const isEveryWordShorterThan7 = (words) => {
-  for(let i =0; i < words.length; i++){
-    if(words[i].length < 7){
-      return true
-    }
-  }
+return words.every(word => word.length < 7);
 };
 
 // Filter
 
 const arrayLessThan5 = (nums) => {
-  let array = []
-  for(let i =0; i < nums.length; i++){
-    if(nums[i] < 5 ){
-      array.push(nums[i])
-    }
-  }
-  return array
+  return nums.filter(num => num < 5);
 };
 
 const arrayOddLengthWords = (words) => {
-  let array = []
-  for(let i = 0; i < words.length; i++){
-    if(words[i].length % 2 !== 0){
-      array.push(words[i])
-    }
-  }
-  return array
+  return words.filter(word => word.length % 2 === 1);
 };
-
 // Find
 
 const firstValDivisibleBy4 = (nums) => {
-
-  for(let i =0; i < nums.length; i++){
-    if(nums[i] % 4 === 0 ){
-      return nums[i]
-    }
-  }
-  
+  return nums.find(num => num % 4 === 0);
 };
 
 const firstWordLongerThan4Char = (words) => {
-  
-  for(let i =0; i < words.length; i++){
-    let letterArray = [...words[i]]
-    if(letterArray.length > 4){
-      return words[i]
-    }
-  }
+  return words.find(word => word.length > 4);
 };
-
 // Find Index
 
 const firstNumIndexDivisibleBy3 = (nums) => {
-  for(let i =0; i < nums.length; i++){
-    if(nums[i] % 3 === 0){
-      return i
-    }
-  }
+  return nums.findIndex((num) => num % 3 === 0);
 };
 
 const firstWordIndexLessThan2Char = (words) => {
-  for(let i =0; i < words.length; i++){
-    if(words[i].length < 2){
-      return i
-    }
-  }
-
+  return words.findIndex(word => word.length < 2);
 };
-
 // For Each
 
 const logValuesTimes3 = (nums) => {
-  for(let i =0; i < nums.length; i++){
-    console.log(nums[1] * 3);
-  }
+  return nums.forEach(num => console.log( num * 3));
 };
 
 const logWordsWithExclamation = (words) => {
-  for(let i =0; i < words.length; i++){
-    console.log(`${words[i]}!`);
-  }
+  return words.forEach(word => console.log(word + '!'));
 };
 
 // Map
-
+//the square root is just the same number multiplied ------> (word * word)
 const arrayValuesSquaredTimesIndex = (nums) => {
-  let array = []
-  for(let i = 0; i < nums.length; i++){
-    let newNumber = i * (nums[i] * nums[i])
-    array.push(newNumber)
-  }
-  return array
-
+  return nums.map((num, i) => { return i * (num * num)});
 };
 
 const arrayWordsUpcased = (words) => {
-  let array = []
-  for(let i = 0; i < nums.length; i++){
-    let upperCased = words[i].toUpperCase()
-    array.push(upperCased)
-  }
-  return array
+  return words.map(word => word.toUpperCase());
 };
-
 // Some
 
 const areSomeNumsDivisibleBy7 = (nums) => {
-  for(let i =0; i < nums.length; i++){
-    if(nums[i] % 7 === 0 ){
-      return true
-    }
-  }
+  return nums.some(num => num % 7 === 0);
 };
 
 const doSomeWordsHaveAnA = (words) => {
-  for(let i =0; i < words.length; i++){
-    let word = [...words[i]]
-    for(let j = 0; j < word.length; j++){
-      console.log(word[j])
-      
-      if(word[j] === 'a' ){
-        return true
-      }
-    }
-  }
+  return words.some(word => word.includes("a"));
 };
-
 module.exports = {
   isEveryNumGreaterThan2,
   isEveryWordShorterThan7,
