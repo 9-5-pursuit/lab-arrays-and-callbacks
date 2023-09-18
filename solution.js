67f229ff0310350b87431eade30bfa72a8b8d095
Irfan Hanjra <ihanjra@gmail.com>
2023-09-18 00:33:06 -0400

    Initial commit

--- solution.js
+++ solution.js
@@ -1,73 +1,54 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
-};
+const isEveryNumGreaterThan2 = (nums) => nums.every
+(element => element >= 2); //inline arrow function
+  
 
-const isEveryWordShorterThan7 = () => {
-  //
-};
+const isEveryWordShorterThan7 = (words) => words.every(element => element.length < 7);
 
 // Filter
+//returns array
+const arrayLessThan5 = (nums) => nums.filter(num => num < 5);
 
-const arrayLessThan5 = () => {
-  //
-};
 
-const arrayOddLengthWords = () => {
-  //
-};
+const arrayOddLengthWords = (words) => words.filter(word => word.length % 2 !== 0);
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-  //
-};
+const firstValDivisibleBy4 = (nums) => nums.find
+(num => num % 4 === 0)
 
-const firstWordLongerThan4Char = () => {
-  //
-};
+const firstWordLongerThan4Char = (words) => words.find(word => word.length > 4)
+
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
-  //
-};
+const firstNumIndexDivisibleBy3 = (nums) => nums.findIndex(num => num % 3 === 0)
 
-const firstWordIndexLessThan2Char = () => {
-  //
-};
+const firstWordIndexLessThan2Char = (words) => words.findIndex(word => word.length < 2)
 
 // For Each
 
-const logValuesTimes3 = () => {
-  //
-};
+const logValuesTimes3 = (nums) => nums.forEach(num => console.log(num * 3))
 
-const logWordsWithExclamation = () => {
-  //
-};
+const logWordsWithExclamation = (words) => words.forEach(word => console.log(`${word}!`))
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
+const arrayValuesSquaredTimesIndex = (nums) => {
+  return nums.map((num, index) => num ** 2 * index)
 };
 
-const arrayWordsUpcased = () => {
-  //
+const arrayWordsUpcased = (words) => {
+  return words.map((word) => word.toUpperCase())
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
-  //
-};
+const areSomeNumsDivisibleBy7 = (nums) => nums.some(num => num % 7 === 0)
 
-const doSomeWordsHaveAnA = () => {
-  //
-};
+const doSomeWordsHaveAnA = (words) => words.some(word => word.includes("a"))
 
 module.exports = {
   isEveryNumGreaterThan2,
