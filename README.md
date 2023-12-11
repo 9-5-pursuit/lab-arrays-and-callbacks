66faa4ec66f129047e7e6fe4b31976e65f51657d
anickacodes <anickalewis@pursuit.org>
2023-12-11 16:02:59 -0500

    practice ✅

--- README.md
+++ README.md
@@ -164,7 +164,70 @@ const panagram = [
 #### Working with data
 
 - filter for products with a price that is less than 10, using the array below:
+[
+      {
+        name: 'fairy lights',
+        price: 5.99,
+        description: 'festive holiday decoration'
+      },
+      { name: 'banana', price: 0.99, description: 'full of potassium' },
+      {
+        name: 'egg separator',
+        price: 3.99,
+        description: 'it separates yolks from whites'
+      },
+      { name: 'flag', price: 5.99, description: 'catches the breeze' },
+      { name: 'quark', price: 0.01, description: 'Very small' },
+      {
+        name: 'yoyo',
+        price: 1,
+        description: 'We had to pull some strings to get this one in.'
+      }
+]
+
 - sort alphabetically by product name
+
+sort by name [
+      { name: 'banana', price: 0.99, description: 'full of potassium' },
+      {
+        name: 'egg separator',
+        price: 3.99,
+        description: 'it separates yolks from whites'
+      },
+      {
+        name: 'fairy lights',
+        price: 5.99,
+        description: 'festive holiday decoration'
+      },
+      { name: 'flag', price: 5.99, description: 'catches the breeze' },
+      {
+        name: 'mitt (leather)',
+        price: 15,
+        description: 'regulation sized'
+      },
+      {
+        name: 'nothing',
+        price: 10,
+        description: "Hey, if you pay us, we won't ask any questions."
+      },
+      { name: 'quark', price: 0.01, description: 'Very small' },
+      {
+        name: 'turtleneck',
+        price: 19.99,
+        description: 'available in black and slightly-darker black'
+      },
+      {
+        name: 'violin',
+        price: 2000,
+        description: 'Talk about a JS fiddle...'
+      },
+      {
+        name: 'yoyo',
+        price: 1,
+        description: 'We had to pull some strings to get this one in.'
+      }
+]
+
 
 ```js
 const products = [
--- solution.js
+++ solution.js
@@ -51,6 +51,8 @@ console.log('sum', sumOfNums)
 const concatWords = words.reduce((accumulator, current)=> accumulator + current, '')
 console.log('concat', concatWords)
 
+
+
 myNums = [...nums]
 
 const sortedNums = myNums.sort()
@@ -71,6 +73,93 @@ console.log(SortWordsArrAsc(myWords))
 
 const SortWordsArrDsc = () => myWords.sort((a,b) => b.localeCompare(a))
 console.log('words decend',SortWordsArrDsc(myWords))
+
+
+const panagram = [
+  "The",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+const isPanagram = () => {
+  const letters = Array(26).fill(false)
+  for (const word of panagram) {
+   for (const letter of word.toLowerCase()){
+    const charCode = letter.charCodeAt(0) - 'a'.charCodeAt(0);
+    if (charCode >= 0 && charCode < 26) {
+      letters[charCode] = true;
+    }
+  }
+}
+const isPanagramResult = letters.every(used => used);
+console.log('isPanagram', isPanagramResult);
+}
+isPanagram()
+// true
+
+const products = [
+  {
+    name: "fairy lights",
+    price: 5.99,
+    description: "festive holiday decoration",
+  },
+  {
+    name: "banana",
+    price: 0.99,
+    description: "full of potassium",
+  },
+  {
+    name: "egg separator",
+    price: 3.99,
+    description: "it separates yolks from whites",
+  },
+  {
+    name: "flag",
+    price: 5.99,
+    description: "catches the breeze",
+  },
+  {
+    name: "quark",
+    price: 0.01,
+    description: "Very small",
+  },
+  {
+    name: "turtleneck",
+    price: 19.99,
+    description: "available in black and slightly-darker black",
+  },
+  {
+    name: "mitt (leather)",
+    price: 15,
+    description: "regulation sized",
+  },
+  {
+    name: "nothing",
+    price: 10,
+    description: "Hey, if you pay us, we won't ask any questions.",
+  },
+  {
+    name: "violin",
+    price: 2000,
+    description: "Talk about a JS fiddle...",
+  },
+  {
+    name: "yoyo",
+    price: 1,
+    description: "We had to pull some strings to get this one in.",
+  },
+];
+
+const filterProductsForPrice = () => products.filter(product => product.price < 10)
+console.log(filterProductsForPrice())
+
+const sortProductsByName = () => [...products].sort((a,b) => a.name.localeCompare(b.name))
+console.log('sort by name',sortProductsByName())
 
 module.exports = {
   isEveryNumGreaterThan2,
